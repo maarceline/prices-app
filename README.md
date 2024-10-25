0d304e1ce70d7bd4558ab1fdb42c57b5ca9a4c57
maarceline <maarceline00@gmail.com>
2024-10-25 21:32:39 +0200

    base project

---
+++ README.md
@@ -0,0 +1,3 @@
+# prices_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/prices_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prices_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
